445993b61e3f4ba2d8bfdf7a7cd0e7015fd18d5b
gluisjulian <gluisjulian@gmail.com>
2023-02-28 16:29:58 -0300

    Aula 5 - Imutabilidade e manipulação do estado

--- treinamento_react/src/components/MedicoList.js
+++ treinamento_react/src/components/MedicoList.js
@@ -11,12 +11,26 @@ class MedicoList extends Component {
                 'Médico 4',
                 'Médico 5',
                 'Dr. House'
-            ]
+            ],
+            novoItem: null
+        }
+
+        handleChanged = (e) => {
+            this.setState({
+                novoItem: e.target.value
+            })
+        }
+
+        handleSubmit = (e) => {
+            this.setState({
+                medicos:[...this.state.medicos, this.state.novoItem]
+            })
         }
 
 
     render()
     {
+        console.log(this.state.novoItem)
         return(
             <>
                 <span><strong>Centro de custo: {this.props.cc}</strong></span>
@@ -24,6 +38,9 @@ class MedicoList extends Component {
                     <li> { this.props.especialidade } </li>
                     { this.state.medicos.map(medico => <li key={medico}> {medico}</li>) }
                 </ul>
+
+                <input type="text" onChange={this.handleChanged}/>
+                <button type="submit" onClick={this.handleSubmit}>Gravar</button>
             </>
         )
     }
